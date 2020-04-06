6c99c2c3d3b1ea984c6457aa9ff86c21c7482583
Luis Alonso Murillo Rojas <luis.murillo@ridgerun.com>
2020-04-06 07:42:37 -0600

    Add reconstruction test of the AAD

--- run_model.py
+++ run_model.py
@@ -4,19 +4,34 @@ import sys
 import argparse
 
 from models.DCGAN import DCGAN
+from models.AAD import AAD
 
 #----------------------------------------------------------------------------
 
-def generate_samples(generatorDir, discriminatorDir, dataDir, resultsDir):
+def generate_samples(generatorDir, discriminatorDir, latentDim, dataDir, resultsDir):
     modelDir = {
         "generator": generatorDir,
         "discriminator": discriminatorDir
     }
 
-    gan  = DCGAN(100, resultsDir)
+    gan  = DCGAN(latentDim, resultsDir)
     gan.load(modelDir)
     gan.preprocessing(dataDir)
     gan.plot()
+
+def evaluate_anomaly(generatorDir, discriminatorDir, anomalyDetectorDir, latentDim, dataDir, resultsDir):
+    modelDir = {
+        "generator": generatorDir,
+        "discriminator": discriminatorDir
+    }
+
+    gan  = DCGAN(latentDim, resultsDir)
+    gan.load(modelDir)
+
+    anomaly_detector = AAD(gan.get_generator(), gan.get_discriminator(), resultsDir, latentDim)
+    anomaly_detector.load(anomalyDetectorDir)
+    anomaly_detector.preprocessing(dataDir)
+    anomaly_detector.plot()
 
 #----------------------------------------------------------------------------
 
@@ -37,6 +52,16 @@ def cmdline(argv):
 
     p.add_argument(     '--generatorDir',       help='Path of the GAN\'s generator weights', default='')
     p.add_argument(     '--discriminatorDir',   help='Path of the GAN\'s discriminator weights', default='')
+    p.add_argument(     '--latentDim',          help='Latent dimension of the GAN', type=int, default=100)
+    p.add_argument(     '--dataDir',            help='Path of the dataset', default='')
+    p.add_argument(     '--resultsDir',         help='Path where the results will be stored', default='')
+
+    p = add_command(    'evaluate_anomaly',     'Remove anomalies from some input image.')
+
+    p.add_argument(     '--generatorDir',       help='Path of the GAN\'s generator weights', default='')
+    p.add_argument(     '--discriminatorDir',   help='Path of the GAN\'s discriminator weights', default='')
+    p.add_argument(     '--anomalyDetectorDir', help='Path of the AnomalyDetector weights', default='')
+    p.add_argument(     '--latentDim',          help='Latent dimension of the GAN', type=int, default=100)
     p.add_argument(     '--dataDir',            help='Path of the dataset', default='')
     p.add_argument(     '--resultsDir',         help='Path where the results will be stored', default='')
 
